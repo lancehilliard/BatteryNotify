9c1d2a5be853874c96f9e5ac2f5a0c7d77d67b0b
lancehilliard <lance@digitalcreations.cc>
2018-01-29 09:50:00 -0600

    initial commit

---
+++ BatteryNotify.WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace BatteryNotify.WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitContextMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitContextMenuItem
+            // 
+            this.exitContextMenuItem.Name = "exitContextMenuItem";
+            this.exitContextMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitContextMenuItem.Text = "E&xit";
+            this.exitContextMenuItem.Click += new System.EventHandler(this.exitContextMenuItem_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "Form1";
+            this.Text = "BatteryNotify";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem exitContextMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ BatteryNotify.WindowsFormsApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BatteryNotify.WindowsFormsApp
+{
+    public partial class Form1 : Form {
+        private readonly int IconTooltipMaxLength = 64;
+        private readonly WebClient _webClient = new WebClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            Size = new Size(0, 0);
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+            Hide();
+            
+            ThreadPool.QueueUserWorkItem(state => {
+                var tempFileName = Path.GetTempFileName();
+                while (true) {
+                    try {
+                        var batteryRemainingPercentage = (int)(SystemInformation.PowerStatus.BatteryLifePercent*100);
+                        batteryRemainingPercentage = batteryRemainingPercentage < 100 ? batteryRemainingPercentage : 100;
+                        batteryRemainingPercentage = batteryRemainingPercentage > 0 ? batteryRemainingPercentage : 0;
+                        _webClient.DownloadFile($"http://home.digitalcreations.cc:8468/NumberDisplayer/{batteryRemainingPercentage}", tempFileName);
+                        notifyIcon.Text = $@"{DateTime.Now}";
+                    }
+                    catch (Exception exception) {
+                        notifyIcon.Text = exception.Message.Substring(0, exception.Message.Length < IconTooltipMaxLength ? exception.Message.Length : IconTooltipMaxLength - 1);
+                    }
+                    Thread.Sleep(60000);
+                }
+                // ReSharper disable once FunctionNeverReturns
+            });
+        }
+
+        private void exitContextMenuItem_Click(object sender, EventArgs e)
+        {
+            _webClient.Dispose();
+            Close();
+        }
+    }
+}
